64075e795bd95a188b98f61262f4d268b6a8dbdd
duc-dev <dcmlrduccc@gmail.com>
2026-05-07 21:35:30 +0700

    hoan thanh bai 2

---
+++ src/TinhHinhChuNhat/TinhHinhChuNhat.java
@@ -0,0 +1,13 @@
+package TinhHinhChuNhat;
+
+public class TinhHinhChuNhat {
+
+	public static void main(String[] args) {
+		int chieudai = 100000;
+		int chieurong = 1000000;
+		int chuvi = (chieudai+chieurong)*2;
+		int dientich = chieudai*chieurong;
+		System.out.println("chu vi hinh chu nhat :" + chuvi);
+		System.out.println("dien tich hinh chu nhat :" + dientich);
+	}
+}
---
+++ src/ex1/Main.java
@@ -0,0 +1,12 @@
+package ex1;
+
+public class Main {
+	public static void main(String[] args) {
+    int age = 16;
+    if( age >=18 ) {
+    	System.out.println( "du dieu kien lai xe");
+    } else {
+    	System.out.println("chua du dieu kien lai xe");
+    }
+	}
+}
