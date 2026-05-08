e5af0fb6ce4e83ee087def081f842155a446e20f
duc-dev <dcmlrduccc@gmail.com>
2026-05-08 08:23:25 +0700

    bai 4

---
+++ src/bai5/bai5.java
@@ -0,0 +1,12 @@
+package bai5;
+public class bai5 {
+	public static void main(String[] args) {
+		int i = 1;
+		int tong = 0;
+		while(i <= 50) {
+			tong = tong + i;
+			i++;
+		}
+		System.out.println("tong la: " + tong);
+	}
+}
