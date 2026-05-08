a1eef175c4ad54678ce19e33f516139ec53dff82
duc-dev <dcmlrduccc@gmail.com>
2026-05-08 07:49:33 +0700

    bai tap 2

---
+++ src/bai3/bai3.java
@@ -0,0 +1,11 @@
+package bai3;
+public class bai3 {
+	public static void main(String[] args) {
+		for(int i = 1 ; i<=20;i++) {
+			System.out.println("số thứ tự: " + i);
+		}
+		for(int i = 2 ; i <=20 ; i+=2 ) {
+			System.out.println("số chẵn: " + i);
+		}
+	}
+}
